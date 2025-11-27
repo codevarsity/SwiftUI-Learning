ad95118ff672e7c3e3ab85cf0b71dca5f10d056a
Shruti <shruti.sachdeva@galgotiasuniversity.edu.in>
2025-11-27 19:26:08 +0530

    Destination name added to destination card view as overlay on the image.

--- TravelDestinations-SwiftUI/TravelDestinations-SwiftUI/DestinationCardView.swift
+++ TravelDestinations-SwiftUI/TravelDestinations-SwiftUI/DestinationCardView.swift
@@ -13,6 +13,19 @@ struct DestinationCardView : View {
     var height:CGFloat
     var body: some View {
         Image("\(destination.imageName)").resizable().frame(width:width, height: height).scaledToFill().clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay(alignment: .bottomLeading) {
+                    Text(destination.name)
+                    .font(.subheadline)
+                    .padding(5)
+                    .background {
+                        Rectangle()
+                            .foregroundStyle(.white)
+                            .opacity(0.4)
+                    }
+            }
     }
 }
 
+#Preview {
+    DestinationCardView(destination: DestinationsData().destinations[2], width: 400, height: 300)
+}
